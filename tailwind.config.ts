6d2d1cff136290fc27caf0ae17d345073e63565d
warishanaz45 <164610323+warishanaz45@users.noreply.github.com>
2024-12-08 20:33:25 +0500

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        IntegralCF: ['Integral CF', 'sans-serif'],
+        Satoshi: ['Satoshi', 'sans-serif'],
+      },
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
